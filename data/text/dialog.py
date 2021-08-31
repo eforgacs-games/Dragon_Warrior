273093ab7497cd62b69e39b9fea273c0cd35e0b3
Edward Forgacs <eddie.forgacs@gmail.com>
2021-08-30 21:10:28 -0400

    Fix collision and talk function

--- data/text/dialog.py
+++ data/text/dialog.py
@@ -44,12 +44,16 @@ class Dialog:
 
     def roaming_guard_tantegel_throne_room(self):
         # TODO: Fix roaming guard talk function. Only able to talk to the roaming guard's initial position.
-        know_about_princess = input("'Dost thou know about Princess Gwaelin?'")
-        if not know_about_princess:
-            print_with_beep_sfx("'Half a year now hath passed since the Princess was kidnapped by the enemy.'")
-            print_with_beep_sfx("'Never does the King speak of it, but he must be suffering much.'")
-        else:
-            print_with_beep_sfx(f"'{self.player.name}, please save the Princess.'")
+        # know_about_princess = input("'Dost thou know about Princess Gwaelin?'")
+        # if not know_about_princess:
+        #     print_with_beep_sfx("'Half a year now hath passed since the Princess was kidnapped by the enemy.'")
+        #     print_with_beep_sfx("'Never does the King speak of it, but he must be suffering much.'")
+        # else:
+        #     print_with_beep_sfx(f"'{self.player.name}, please save the Princess.'")
+        # TODO: Reset this later to use input(), but just for testing it out we can leave it this way for now.
+        print_with_beep_sfx("'Dost thou know about Princess Gwaelin?'")
+        print_with_beep_sfx("'Half a year now hath passed since the Princess was kidnapped by the enemy.'")
+        print_with_beep_sfx("'Never does the King speak of it, but he must be suffering much.'")
 
     @staticmethod
     def left_face_guard_tantegel_throne_room():
