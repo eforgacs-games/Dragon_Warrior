c015d04b288effc59cb77a200f69607ebe0158ed
Edward Forgacs <eddie.forgacs@gmail.com>
2022-04-30 19:19:07 +0200

    Add Tantegel Courtyard wise man dialog

--- data/text/wise_man_dialog.py
+++ data/text/wise_man_dialog.py
@@ -5,6 +5,12 @@ class WiseManDialog(Dialog):
     def __init__(self, player, map_name):
         super().__init__(player, None)
         match map_name:
+            case 'TantegelCourtyard':
+                self.dialog_text = (
+                    f"'{self.player.name}'s coming was foretold by legend. May the light shine upon this brave warrior.'",
+                )
+            #     make screen flash 8 times
+                self.player.current_mp = self.player.max_mp
             case 'Brecconary':
                 self.dialog_text = (
                     "'If thou art cursed, come again.'",
