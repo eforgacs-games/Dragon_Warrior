c99c1e2af7768bd548150d6e234e03367b9478fb
Edward Forgacs <eddie.forgacs@gmail.com>
2022-05-20 15:36:14 +0200

    Delete map_lookups.py

--- src/map_lookups.py
+++
@@ -1,7 +0,0 @@
-from src.maps import TantegelThroneRoom, TantegelCourtyard
-
-# TODO(ELF): Add last map and coordinates, so that we can put the player
-#  in a sensible place after map changes
-last_map_lookup = {
-    TantegelThroneRoom: {TantegelCourtyard: (5, 5)},
-}
