6119e08793446397a91ffbf483b3162ce795caac
Edward Forgacs <eddie.forgacs@gmail.com>
2022-06-07 21:26:49 +0200

    Add tests

---
+++ tests/test_camera.py
@@ -0,0 +1,43 @@
+import os
+from unittest import TestCase
+
+import pygame
+from pygame.display import set_mode
+
+from src.camera import Camera
+from src.common import Direction
+from src.config import NES_RES, SCALE
+from src.game import Game
+from src.maps import MapWithoutNPCs
+
+os.environ["SDL_VIDEODRIVER"] = "dummy"
+os.environ['SDL_AUDIODRIVER'] = 'dummy'
+
+layout = [[33, 0, 3],
+          [1, 2, 3],
+          [3, 3, 39]]
+
+
+class MockMap(MapWithoutNPCs):
+    __test__ = False
+
+    def __init__(self):
+        super().__init__(layout)
+
+    def hero_underlying_tile(self):
+        return 'BRICK'
+
+    def hero_initial_direction(self):
+        return Direction.DOWN.value
+
+
+class TestCamera(TestCase):
+    def setUp(self) -> None:
+        self.game = Game()
+        self.game.camera_pos = 0, 0
+        self.center_pt = 0, 0
+
+    def test_set_pos(self):
+        self.game.camera.set_pos((1, 2))
+        self.assertEqual(1, self.game.camera.x)
+        self.assertEqual(2, self.game.camera.y)
