967db67e6415f7f341d782044f0ea01a0c42a244
Edward Forgacs <eddie.forgacs@gmail.com>
2022-06-11 12:30:43 +0200

    Fix LGTM alert: First argument to super() is not enclosing class

--- src/maps.py
+++ src/maps.py
@@ -182,7 +182,7 @@ class DragonWarriorMap:
         if current_tile == self.character_key['HERO']['val']:
             # TODO(ELF): not the greatest thing to call the constructor every single time the character is mapped.
             #  instead, it should just pass the player object from map to map and keep all the attributes.
-            super(Player, player).__init__(self.center_pt, player.direction_value, self.scale_sprite_sheet(UNARMED_HERO_PATH), identifier='HERO')
+            AnimatedSprite.__init__(player, self.center_pt, player.direction_value, images=self.scale_sprite_sheet(UNARMED_HERO_PATH), identifier='HERO')
             self.map_player(character_dict['underlying_tile'], player, coordinates)
         else:
             self.map_npc(character, character_dict.get('direction'), character_dict['underlying_tile'], character_dict['path'], character_dict['four_sided'],
