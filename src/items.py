bf2aa2e23ff0dbd876056b67c49495e5b8abffe0
Edward Forgacs <eddie.forgacs@gmail.com>
2022-06-28 14:41:46 +0200

    Add Erdrick's Tablet

--- src/items.py
+++ src/items.py
@@ -32,5 +32,7 @@ shields = {
 treasure = {
     'TantegelThroneRoom': {(7, 17): {'item': 'Magic Key'},
                            (10, 14): {'item': "GOLD", 'amount': 120},
-                           (10, 15): {'item': "Torch"}}
+                           (10, 15): {'item': "Torch"}},
+    'ErdricksCaveB2': {(4, 10): {'item': 'Tablet'}}
+
 }
