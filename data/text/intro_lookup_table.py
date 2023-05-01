a074800fefeb05dda754ed5c55f664dc3f6441ff
Edward Forgacs <eddie.forgacs@gmail.com>
2023-05-01 09:53:56 +0100

    Add partial i18n of intro in Korean, and more i18n infrastructure

---
+++ data/text/intro_lookup_table.py
@@ -0,0 +1,20 @@
+import gettext
+import os
+
+from src.config import LANGUAGE
+
+if LANGUAGE == 'Korean':
+    ko = gettext.translation('base', localedir=os.path.join('../data/text/locales'), languages=['ko'])
+    ko.install()
+    _ = ko.gettext
+else:
+    _ = gettext.gettext
+
+push_start = _("-PUSH START-")
+a_button = _("K key: A Button")
+b_button = _("J key: B Button")
+start_button = _("I key: Start")
+arrow_keys_move_buttons = _("WASD / Arrow Keys: Move")
+controls = a_button, b_button, start_button, arrow_keys_move_buttons
+input_name_prompt = _("Type your name using the keyboard.\n"
+                      "If you are using a joystick, press the TAB key to switch to joystick input.")
