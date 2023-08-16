66e9980ca8dc31bf8111a47a2a5c1a9032c5d15f
Edward Forgacs <eddie.forgacs@gmail.com>
2023-08-16 23:58:36 +0200

    Remove redundant MockMap definitions

--- tests/test_camera.py
+++ tests/test_camera.py
@@ -2,30 +2,11 @@ import os
 from unittest import TestCase
 from unittest.mock import patch
 
-from src.direction import Direction
 from src.config import prod_config
 from src.game import Game
-from src.maps import MapWithoutNPCs
 
 os.environ['SDL_VIDEODRIVER'] = 'dummy'
 os.environ['SDL_AUDIODRIVER'] = 'dummy'
-
-layout = [[33, 0, 3],
-          [1, 2, 3],
-          [3, 3, 39]]
-
-
-class MockMap(MapWithoutNPCs):
-    __test__ = False
-
-    def __init__(self):
-        super().__init__(layout, None, None, ())
-
-    def hero_underlying_tile(self):
-        return 'BRICK'
-
-    def hero_initial_direction(self):
-        return Direction.DOWN.value
 
 
 class TestCamera(TestCase):
